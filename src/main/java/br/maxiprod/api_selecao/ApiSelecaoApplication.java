6b37a3fe892fb4a6aadfe734834e73ef589f51a5
Guilherme Diniz dos Reis Gomes <gdrg@discente.ifpe.edu.br>
2025-03-05 20:53:42 -0300

    erro 403

--- src/main/java/br/maxiprod/api_selecao/ApiSelecaoApplication.java
+++ src/main/java/br/maxiprod/api_selecao/ApiSelecaoApplication.java
@@ -1,13 +1,14 @@
 package br.maxiprod.api_selecao;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(servers = {
-		@Server(url = "/", description = "Uma API para um blog de perguntas e respostas") })
+		@Server(url = "/", description = "Uma API") })
 public class ApiSelecaoApplication {
 
 	public static void main(String[] args) {
