4113395ca925584c23ef07066dda9f810b900919
Guilherme Diniz dos Reis Gomes <gdrg@discente.ifpe.edu.br>
2025-03-05 19:37:01 -0300

    mudanças pro railway

--- src/main/java/br/maxiprod/api_selecao/config/WebConfig.java
+++ src/main/java/br/maxiprod/api_selecao/config/WebConfig.java
@@ -1,18 +1,18 @@
 package br.maxiprod.api_selecao.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://spring-blog-production-7940.up.railway.app") // Usando padrão para
-                                                                                             // permitir todas as
-                                                                                             // origens do Railway
+                .allowedOrigins("*") // Permite todas as origens (teste apenas)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
